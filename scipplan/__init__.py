b20f114df92368a0808613d5f023716bd144f80b
Ari Gestetner <ariges770@gmail.com>
2025-07-25 13:45:59 +1000

    Version 0.2.2a0 includes bugfixes to constraint generation and encodings.

--- scipplan/__init__.py
+++ scipplan/__init__.py
@@ -1,5 +1,5 @@
-__version__ = "0.2.1alpha0"
+__version__ = "0.2.2alpha0"
 print(f"SCIPPlan Version: {__version__}")
-__release__ = "v0.2.1a0"
+__release__ = "v0.2.2a0"
 __author__ = "Ari Gestetner, Buser Say"
 __email__ = "ari.gestetner@monash.edu, buser.say@monash.edu"
